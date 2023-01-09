6f78489a0761e0a93cbbc52d39d90f6e84faf71e
flxfix <felixaantai@gmail.com>
2023-01-09 22:28:49 +0000

    ds

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+sible_by_2(my_list=[]):
+    new_list = []
+    for number in my_list:
+        if (number % 2) == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+    return(new_list)
